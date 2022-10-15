7cb33877b5e5cea9a21dc04192d2603ed876fba6
sonikakourav <72182485+sonikakourav@users.noreply.github.com>
2022-10-15 16:36:14 +0530

    Create Abstraction in C++.cpp

---
+++ Abstraction in C++.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+class AbstractionExample{
+private:
+   /* By making these data members private, I have
+    * hidden them from outside world.
+    * These data members are not accessible outside
+    * the class. The only way to set and get their
+    * values is through the public functions.
+    */
+   int num;
+   char ch;
+
+public:
+   }
+   void getMyValues() {
+      cout<<"Numbers is: "<<num<< endl;
+      cout<<"Char is: "<<ch<<endl;
+   }
+};
+int main(){
+   AbstractionExample obj;
+   obj.setMyValues(100, 'X');
+   obj.getMyValues();
+   return 0;
+}
